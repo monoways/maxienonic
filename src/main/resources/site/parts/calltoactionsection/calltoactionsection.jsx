68bb697570e0d2ce2262944669f145a8ca830990
Maximilian Skjønhaug <Maximilian.skjonhaug@ways.no>
2023-01-31 17:20:09 +0100

    Fixed the call to action with proper linking

--- src/main/resources/site/parts/calltoactionsection/calltoactionsection.jsx
+++ src/main/resources/site/parts/calltoactionsection/calltoactionsection.jsx
@@ -4,11 +4,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 export default (props) => {
+    const { menuItems } = props.menuItems
 
-    const handleRedirect = (url) => {
-        console.log(url, 'url, in the calltoaction component');
-        window.location.href = url;
-    }
+    // const handleRedirect = (menuItem) => {
+    //     console.log(url, 'url, in the calltoaction component');
+    //     window.location.href = menuItem.url;
+    // }
+
+    const menuItemsArray = [];
+    // this loops through three levels of menu items and pushes them to the menuItemsArray to get a flat array of all menu items, 
+    // it also adds a layer property to each item
+    menuItems.forEach((item) => {
+        item.layer = 0;
+        menuItemsArray.push(item);
+        if (item.hasChildren) {
+            item.children.forEach((child) => {
+                child.layer = 1;
+                child.father = item.title;
+                menuItemsArray.push(child);
+                if (child.hasChildren) {
+                    child.children.forEach((child2) => {
+                        child2.father = child.title;
+                        child2.layer = 2;
+                        menuItemsArray.push(child2);
+                    })
+                }
+            })
+        }
+    })
+
+    const linkItemUrl = menuItemsArray.find(item => item.title === props.link).url;
 
     return (
         <div className={styles.sectionContainer}>
@@ -18,7 +43,10 @@ export default (props) => {
                 <p className={styles.sectionText}>{props.text2}</p>
             </div>
             <div className={styles.sectionRight}>
-                <FontAwesomeIcon icon={faArrowRight} className={styles.arrow} onClick={() => handleRedirect(props.url)} />
+                {/* <FontAwesomeIcon icon={faArrowRight} className={styles.arrow} onClick={() => handleRedirect(linkItemUrl)} /> */}
+                <a href={linkItemUrl}>
+                    <FontAwesomeIcon icon={faArrowRight} className={styles.arrow} />
+                </a>
             </div>
         </div>
     )
