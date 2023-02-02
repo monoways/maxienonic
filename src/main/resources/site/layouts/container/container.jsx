5bda74c1d2bd91f47bdd474c525209d15a51064c
Maximilian Skjønhaug <Maximilian.skjonhaug@ways.no>
2023-02-02 18:23:33 +0100

    typo

--- src/main/resources/site/layouts/container/container.jsx
+++ src/main/resources/site/layouts/container/container.jsx
@@ -6,7 +6,7 @@ import styles from './container.module.css'
 
 export default function container(props) {
     return (
-        <div style={{bacgroundColor:`${props.bacgroundColor}`}} class={styles.containerLayout}>
+        <div style={{backgroundColor:`${props.bacgroundColor}`}} class={styles.containerLayout}>
             <Regions {...props} />
         </div>
     )
