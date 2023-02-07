668be54c16affc2edb7952deeec8775021f2cae1
Maximilian Skjønhaug <Maximilian.skjonhaug@ways.no>
2023-02-07 12:34:43 +0100

    added the link to the logos

--- src/main/resources/site/parts/logoandlink/logoandlink.jsx
+++ src/main/resources/site/parts/logoandlink/logoandlink.jsx
@@ -7,11 +7,11 @@ export default (props) => {
     const logoandlink = (
         <div className={styles.logoContainer}>
             <div className={styles.logoImageContainer}>
-                <img className={styles.logoImage} src={props.logo} alt="poster image" />
+                <a href={props.link}><img className={styles.logoImage} src={props.logo} alt="poster image" /></a>
             </div>
             <div className={styles.logoTextContainer}>
                 <p className={styles.logoText}>{props.text}</p>
-                {props.visLink ? <a className={styles.link} href={props.link}>{props.title}</a> : null}
+                {props.visLink ? <a className={styles.link} href={props.link} target='blank'>{props.title}</a> : null}
             </div>
         </div>
     )
